f60bf6cd224403e1b221b35aeca05e240fdecc05
Faaiz <faaizaslam75@live.com>
2022-08-30 23:00:46 +0500

    Fitness App
    Fitness app using Rapid API

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'cc3e1753damshb681927bf0fc226p18010fjsnd87c9226ce86',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'cc3e1753damshb681927bf0fc226p18010fjsnd87c9226ce86',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
